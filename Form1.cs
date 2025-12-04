1da96cd07ccf95accb2391cd7dee496ad7b0ba16
FlakeyCheese <rgblackett@gmail.com>
2025-12-04 14:48:20 +0000

    lots done

--- Form1.cs
+++ Form1.cs
@@ -4,20 +4,31 @@ namespace CardGame
     {
         string imageFolder = @"..\..\..\cards";
         public static Card[] deck = new Card[52];
+        public static Row? row1;
+        public static Row? row2;
+        public static Row? row3;
+        public static Row? row4;
         public Form1()
         {
             InitializeComponent();
+            row1 = new Row(this,50);            
+            row2 = new Row(this,200);            
+            row3 = new Row(this,350);            
+            row4 = new Row(this,500);
+            DealCards();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
             Card[] deck = CardLoader.LoadCards(imageFolder);
-            shuffleDeck();
+            ShuffleDeck();
+            DealCards();
 
         }
 
-        private void shuffleDeck()//a durstenfeld shuffle of the deck
+        private void ShuffleDeck()//a durstenfeld shuffle of the deck
         {
             Random rnd = new Random();
             int currentIndex = 0;
@@ -25,15 +36,24 @@ namespace CardGame
             while (currentIndex < length)
             {
                 int randomIndex = rnd.Next(currentIndex, length);
-                swap(currentIndex, randomIndex);
+                Swap(currentIndex, randomIndex);
                 currentIndex++;
             }
         }
-        private void swap(int currentIndex,int randomIndex)//method to swap two cards
+        private void Swap(int currentIndex,int randomIndex)//method to swap two cards
         {
             Card temp = deck[currentIndex];
             deck[currentIndex] = deck[randomIndex];
             deck[randomIndex] = temp;
+        }
+        private void DealCards()
+        {
+           for (int i = 0; i < 12; i++)
+            {    row1.InitialSetCards(deck[i]);
+                 row2.InitialSetCards(deck[i + 13]);
+                 row3.InitialSetCards(deck[i + 26]);
+                 row4.InitialSetCards(deck[i + 39]);
+            }
         }
     }
 }
--- Row.cs
+++ Row.cs
@@ -9,17 +9,32 @@ namespace CardGame
     public class Row
     {
         public Card[] cardsInRow;
+        public PictureBox[]? pictureBoxes;
         int currentCardIndex = 1;//track the index of the next available position in the row
-
-        public Row()
+        Form1 form; //reference to the main form to add picture boxes
+        public Row(Form1 form1, int yPosition)
         {
             cardsInRow = new Card[14];
+            form = form1;
+            PictureBox[] pictureBoxes = new PictureBox[14];
+            for (int i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxes[i] = new PictureBox
+                {
+                    Size = new Size(100, 140),
+                    Location = new Point(i*100 +10, yPosition),
+                    BorderStyle = BorderStyle.FixedSingle,
+                };
+                form.Controls.Add(pictureBoxes[i]);
+            }
         }
 
         public void InitialSetCards(Card card)
         {
-            cardsInRow[currentCardIndex] = card;
             currentCardIndex++;
+            cardsInRow[currentCardIndex] = card;
+            
         }
+       
     }
 }
