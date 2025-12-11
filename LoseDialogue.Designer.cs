381b762d36540b7361b425bca821d5d77affc448
FlakeyCheese <89859893+FlakeyCheese@users.noreply.github.com>
2025-12-11 20:31:25 +0000

    fixed game over resizing

--- LoseDialogue.Designer.cs
+++ LoseDialogue.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // btnPlayAgain
             // 
-            btnPlayAgain.Location = new Point(148, 70);
+            btnPlayAgain.Location = new Point(169, 93);
+            btnPlayAgain.Margin = new Padding(3, 4, 3, 4);
             btnPlayAgain.Name = "btnPlayAgain";
-            btnPlayAgain.Size = new Size(75, 23);
+            btnPlayAgain.Size = new Size(102, 31);
             btnPlayAgain.TabIndex = 1;
             btnPlayAgain.Text = "Play Again";
             btnPlayAgain.UseVisualStyleBackColor = true;
@@ -46,9 +47,10 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(148, 126);
+            btnExit.Location = new Point(169, 159);
+            btnExit.Margin = new Padding(3, 4, 3, 4);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(75, 23);
+            btnExit.Size = new Size(102, 31);
             btnExit.TabIndex = 1;
             btnExit.Text = "Exit";
             btnExit.UseVisualStyleBackColor = true;
@@ -58,9 +60,9 @@
             // 
             lblGameOverMsg.AutoSize = true;
             lblGameOverMsg.Font = new Font("Perpetua Titling MT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblGameOverMsg.Location = new Point(12, 42);
+            lblGameOverMsg.Location = new Point(14, 56);
             lblGameOverMsg.Name = "lblGameOverMsg";
-            lblGameOverMsg.Size = new Size(85, 20);
+            lblGameOverMsg.Size = new Size(106, 24);
             lblGameOverMsg.TabIndex = 2;
             lblGameOverMsg.Text = "message";
             // 
@@ -68,28 +70,31 @@
             // 
             lblScore.AutoSize = true;
             lblScore.Font = new Font("Perpetua Titling MT", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblScore.Location = new Point(12, 9);
+            lblScore.Location = new Point(14, 12);
             lblScore.Name = "lblScore";
-            lblScore.Size = new Size(85, 20);
+            lblScore.Size = new Size(106, 24);
             lblScore.TabIndex = 2;
             lblScore.Text = "message";
             // 
             // LoseDialogue
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(384, 161);
+            AutoSize = true;
+            ClientSize = new Size(439, 215);
             ControlBox = false;
             Controls.Add(lblScore);
             Controls.Add(lblGameOverMsg);
             Controls.Add(btnExit);
             Controls.Add(btnPlayAgain);
             FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "LoseDialogue";
             ShowIcon = false;
             ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
             Text = "GAME OVER";
             TopMost = true;
             ResumeLayout(false);
